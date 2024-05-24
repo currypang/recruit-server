751f7b78734b7a0bcce8abbb86aad0c543fbab2f
romesway <romesway@gmail.com>
2024-05-24 16:01:44 +0900

    로그인 API 완성

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -6,30 +6,31 @@ import 'dotenv/config';
 const router = express.Router();
 
 // 유효성 검사는 후에 joi로 리팩토링 할 것
-router.post('/users', async (req, res, next) => {
+// 회원가입 API
+router.post('/users/sign-up', async (req, res, next) => {
   try {
     const { email, name, password, passwordConfirm } = req.body;
     const existUser = await prisma.users.findFirst({ where: { email } });
     // 회원 정보 중 하나라도 빠진 경우
     if (!email || !name || !password || !passwordConfirm) {
-      res.status(400).json('정보를 모두 입력해 주세요');
+      return res.status(400).json('정보를 모두 입력해 주세요');
     }
     // 이메일 중복
     if (existUser) {
-      res.status(400).json('이미 가입 된 사용자입니다');
+      return res.status(400).json('이미 가입 된 사용자입니다');
     }
     // 이메일 형식이 맞지 않음
     const val = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!val.test(email)) {
-      res.status(400).json('이메일 형식이 올바르지 않습니다.');
+      return res.status(400).json('이메일 형식이 올바르지 않습니다.');
     }
     // 비밀번호가 6자리 미만인 경우
     if (password.length < 6) {
-      res.status(400).json('비밀번호는 6자리 이상이어야 합니다.');
+      return res.status(400).json('비밀번호는 6자리 이상이어야 합니다.');
     }
     // 비밀번호와 비밀번호 확인이 일치하지 않는 경우
     if (password !== passwordConfirm) {
-      res.status(400).json('입력한 두 비밀번호가 일치하지 않습니다.');
+      return res.status(401).json('입력한 두 비밀번호가 일치하지 않습니다.');
     }
     // dotenv 환경변수는 문자열로 반환, salt를 숫자형으로 변환한다.
     // 해싱
@@ -57,12 +58,4 @@ router.post('/users', async (req, res, next) => {
   }
 });
 
-//test
-router.get('/users', async (req, res, next) => {
-  try {
-    res.status(200).json({ message: 'user api' });
-  } catch (err) {
-    next(err);
-  }
-});
 export default router;
