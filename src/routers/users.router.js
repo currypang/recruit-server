c8652485e7a68f6302bd7177112a2b0c304b6bb4
romesway <romesway@gmail.com>
2024-06-10 11:19:23 +0900

    14

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -5,7 +5,7 @@ import { MESSAGES } from '../constants/message.constant.js';
 
 const userRouter = express.Router();
 
-// 내 정보 조회 API, accessToken 인증 미들웨어 사용
+// 내 정보 조회 API, accessToken 인증 미들웨어 사용.
 userRouter.get('/me', validateAccessToken, async (req, res, next) => {
   try {
     // validateAccessToken 에서 password 제외 후 유저정보 전달하게 리팩토링. 아래 코드 필요 없음.
